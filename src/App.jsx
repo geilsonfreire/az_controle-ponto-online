f1a15df4c660fc84977f457110652a2e2012a9d4
geilsonfreire <geilosnfreireleite@gmail.com>
2025-12-24 06:39:48 -0300

    refatora o componente App para incluir AppContent e redirecionar para a página de loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 // Importando Componentes / Bibliotecas React
 import { HashRouter as Router } from 'react-router-dom';
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 //Importando Componentes / Páginas
 import AppRoutes from './routes/routes';
 
+const AppContent = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Redireciona para a página de loading ao iniciar o app
+        navigate('/loading');
+    }, [navigate]);
+
+    return <AppRoutes />;
+}
+
 const App = () => {
+    
     return (
         <Router>
-            <AppRoutes />
+            <AppContent />
         </Router>
     );
 }
